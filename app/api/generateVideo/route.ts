6fdcdae8f4c975209e462915bf5d60e813272d84
清河 <1801537964@qq.com>
2023-11-09 17:52:36 +0800

    second

--- app/api/generateVideo/route.ts
+++ app/api/generateVideo/route.ts
@@ -8,6 +8,7 @@ async function handle(req: NextRequest) {
       data,
     });
     const API_KEY = process.env.X_Api_Key;
+
     const API_URL = "https://api.heygen.com/v2/video/generate";
     const response = await fetch(API_URL, {
       method: "POST",
@@ -23,44 +24,8 @@ async function handle(req: NextRequest) {
         aspect_ratio: JSON.parse(resRuest).data.aspect_ratio,
       }),
     });
-    const res: any = await response.json();
-    if (res.code === 400664) {
-      return NextResponse.json(res, { status: 200 });
-    } else if (res.error === null) {
-      const checkVideoStatus = async () => {
-        const url = `https://api.heygen.com/v1/video_status.get?video_id=${res.data.video_id}`;
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            "X-Api-Key":
-              process.env.X_Api_Key ||
-              "OWUxZDEwNzgxMTc4NDMxMThmZmRlMDQ1Y2FjNjkxNjMtMTY5OTI1MzA0OA==",
-          },
-        });
-        const videoData: any = await response.json();
-
-        if (videoData.code === 100) {
-          const status = videoData.data.status;
-          if (status === "processing" || status === "waiting") {
-            setTimeout(checkVideoStatus, 2000);
-          } else if (status === "completed") {
-            return NextResponse.json(videoData, { status: 200 });
-          } else if (status === "failed") {
-            return NextResponse.json(videoData, { status: 200 });
-          } else {
-            return NextResponse.json(videoData, { status: 200 });
-          }
-        } else {
-          return NextResponse.json(videoData, { status: 200 });
-        }
-      };
-
-      checkVideoStatus();
-    } else {
-      return NextResponse.json(res, { status: 200 });
-    }
-
-
+    const res = await response.json();
+    return NextResponse.json(res, { status: 200 });
   } catch (error: any) {
     console.error(error);
     return NextResponse.json(
---
+++ app/api/videoStatus/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from "next/server";
+import fetch from "node-fetch";
+
+async function handle(req: NextRequest) {
+  try {
+    let data = await req.json();
+    const videoId = data.video_id;
+    const apiKey =
+      process.env.X_Api_Key ||
+      "OWUxZDEwNzgxMTc4NDMxMThmZmRlMDQ1Y2FjNjkxNjMtMTY5OTI1MzA0OA==";
+    const url = `https://api.heygen.com/v1/video_status.get?video_id=${videoId}`;
+
+    while (true) {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      });
+      const videoData: any = await response.json();
+      const status = videoData.data.status;
+
+      if (status === "processing" || status === "waiting") {
+        // 继续等待，延迟一段时间后再次发起请求
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 延迟2秒钟
+      } else if (status === "completed") {
+        return NextResponse.json(videoData, { status: 200 });
+      } else if (status === "failed") {
+        return NextResponse.json(
+          {
+            code: 1,
+            status: "FAIL",
+            msg: "请求失败",
+          },
+          { status: 200 },
+        );
+      } else {
+        return NextResponse.json(
+          {
+            code: 1,
+            status: "FAIL",
+            msg: "请求失败",
+          },
+          { status: 200 },
+        );
+      }
+    }
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        code: 1,
+        status: "FAIL",
+        msg: error.message || "未知错误，请查看服务端日志",
+      },
+      { status: 200 },
+    );
+  }
+}
+
+export const GET = handle;
+export const POST = handle;
